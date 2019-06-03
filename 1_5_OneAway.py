4d83667bf0ace03d70adf4889835fb7de7e15a88
Gavin-Hoang <zizhen.huang@sjsu.edu>
2019-06-02 21:38:42 -0700

    One Away

---
+++ 1_5_OneAway.py
@@ -0,0 +1,25 @@
+def oneStepAway(s1, s2):
+    if len(s1) - len(s2) > 1: 
+        return False
+    
+    s = s1 + s2
+    bitVector = 0
+    count = 0
+    for c in s:
+        num = ord(c) - ord('a')
+        bitVector ^= (1 << num)
+    
+    while bitVector != 0:
+        bitVector &= bitVector - 1
+        count += 1
+        
+    if count == 1 or count == 0:
+        return True
+    elif len(s1) == len(s2) and count == 2:
+        return True
+    else:
+        return False
+
+print(oneStepAway('abcd', 'abcg'))
+print(oneStepAway('pale', 'ple'))
+print(oneStepAway('plae', 'bae'))
